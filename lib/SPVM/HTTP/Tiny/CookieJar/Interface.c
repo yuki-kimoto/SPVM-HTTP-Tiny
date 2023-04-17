d3bcf5ace43e5f1823925e46539d4c38bb12f66a
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-04-17 12:45:00 +0900

    Added HTTP::Tiny::CookieJar::Interface

---
+++ lib/SPVM/HTTP/Tiny/CookieJar/Interface.c
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "HTTP/Tiny/CookieJar/Interface.c";
+
+int32_t SPVM__HTTP__Tiny__CookieJar__Interface__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
