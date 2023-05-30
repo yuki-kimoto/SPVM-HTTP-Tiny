2782564dee8be47e98a0b84e582f0d705c604fde
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-05-30 17:27:05 +0900

    HTTP::Tiny::Headers and HTTP::Tiny::Response are added

---
+++ lib/SPVM/HTTP/Tiny/Headers.pm
@@ -0,0 +1,37 @@
+package SPVM::HTTP::Tiny::Headers;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::HTTP::Tiny::Headers - Short Description
+
+=head1 Description
+
+The HTTP::Tiny::Headers interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface HTTP::Tiny::Headers;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
---
+++ lib/SPVM/HTTP/Tiny/Response.pm
@@ -0,0 +1,37 @@
+package SPVM::HTTP::Tiny::Response;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::HTTP::Tiny::Response - Short Description
+
+=head1 Description
+
+The HTTP::Tiny::Response interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface HTTP::Tiny::Response;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
