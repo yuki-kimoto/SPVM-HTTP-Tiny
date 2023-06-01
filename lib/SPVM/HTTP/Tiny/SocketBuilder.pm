a1f85d75456f3d945715e493443d00814cd2be77
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-01 09:33:56 +0900

    Parital commit

---
+++ lib/SPVM/HTTP/Tiny/SocketBuilder.pm
@@ -0,0 +1,37 @@
+package SPVM::HTTP::Tiny::SocketBuilder;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::HTTP::Tiny::SocketBuilder - Short Description
+
+=head1 Description
+
+The HTTP::Tiny::SocketBuilder interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface HTTP::Tiny::SocketBuilder;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
