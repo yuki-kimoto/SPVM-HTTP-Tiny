da45257a86571241e4616c1fce1e333bbe3296c4
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-01 09:57:25 +0900

    Partial commit

---
+++ lib/SPVM/HTTP/Tiny/HostPort.pm
@@ -0,0 +1,45 @@
+package SPVM::HTTP::Tiny::HostPort;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::HTTP::Tiny::HostPort - Short Description
+
+=head1 Description
+
+The HTTP::Tiny::HostPort class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use HTTP::Tiny::HostPort;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
