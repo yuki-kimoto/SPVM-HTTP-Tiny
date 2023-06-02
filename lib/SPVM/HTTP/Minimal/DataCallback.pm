79e0cf0fb0fc27c18ae475b9140d251406051d88
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-02 14:16:47 +0900

    Partial commit

---
+++ lib/SPVM/HTTP/Minimal/DataCallback.pm
@@ -0,0 +1,37 @@
+package SPVM::HTTP::Minimal::DataCallback;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::HTTP::Minimal::DataCallback - Short Description
+
+=head1 Description
+
+The HTTP::Minimal::DataCallback interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface HTTP::Minimal::DataCallback;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
