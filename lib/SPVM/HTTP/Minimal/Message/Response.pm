46bff31a5ad88fd7220473f06b49b022acbb36c5
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-22 13:30:19 +0900

    The doc of response is improved.

--- lib/SPVM/HTTP/Minimal/Message/Response.pm
+++ lib/SPVM/HTTP/Minimal/Message/Response.pm
@@ -10,20 +10,49 @@ SPVM::HTTP::Minimal::Message::Response - HTTP Response
 
 =head1 Description
 
-The HTTP::Minimal::Message::Response interface of L<SPVM> has methods to manipulate HTTP requests.
+The HTTP::Minimal::Message::Response class of L<SPVM> has methods to manipulate HTTP responses.
 
 =head1 Usage
 
-  interface HTTP::Minimal::Message::Response;
+  use HTTP::Minimal::Message::Response;
+  
+  my $is_success = $res->is_success;
+  
+  my $content = $res->content;
 
-=head1 Interface Methods
+=head1  Fields
 
+=head2 protocol
 
+  has protocol : ro string;
 
+The protocol of the HTTP response.
 
-=head1 Repository
+=head2 status
 
+  has status : ro string;
 
+The status code of the HTTP response.
+
+=head2 reason
+
+  has reason : ro string;
+
+The reason of the status code of the HTTP response.
+
+=head1 Instance Methods
+
+=head2 is_success
+
+  method is_success : int ();
+
+If the response contains successful status code, returns 1. Otherwise returns 0.
+
+=head2 content
+
+  method content : string ();
+
+Returns the content body of the response.
 
 =head1 Copyright & License
 
