693dee785e1f38afb64a913840148ba6b3a21b78
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-22 12:22:44 +0900

    Header doc is improved.

--- lib/SPVM/HTTP/Minimal/Headers.pm
+++ lib/SPVM/HTTP/Minimal/Headers.pm
@@ -10,11 +10,57 @@ SPVM::HTTP::Minimal::Headers - HTTP Headers
 
 =head1 Description
 
-The HTTP::Minimal::Headers class of L<SPVM> has interface methods to manipulate HTTP headers.
+The HTTP::Minimal::Headers class of L<SPVM> has methods to manipulate HTTP headers.
 
 =head1 Usage
 
   my $headers = HTTP::Minimal::Headers->new;
+
+=head1 Class Methods
+
+=head2 new
+
+  static method new : HTTP::Minimal::Headers ();
+
+Creates a new L<HTTP::Minimal::Headers|SPVM::HTTP::Minimal::Headers> object.
+
+=head2 Instance Methods
+
+=head2 add
+
+  method add : void ($name : string, $value : string);
+
+Adds a header.
+
+=head2 remove
+
+  method remove : void ($name : string);
+
+Removes a header.
+
+=head2 get
+
+  method get : string[] ($name : string);
+
+Gets a header value.
+
+=head2 get_as_string
+
+  method get_as_string : string ($name : string);
+
+Gets a header value as a string.
+
+=head2 names
+
+  method names : string[] ();
+
+Gets header names.
+
+=head2 to_string
+
+  method to_string : string ();
+
+Converts all headers to a string.
 
 =head1 Copyright & License
 
