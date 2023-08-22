e5ae16be6f18c047b08882acff2280ab4a186af3
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-22 13:05:30 +0900

    The doc of socket is improved.

--- lib/SPVM/HTTP/Minimal/Socket.pm
+++ lib/SPVM/HTTP/Minimal/Socket.pm
@@ -4,36 +4,15 @@ package SPVM::HTTP::Minimal::Socket;
 
 =head1 Name
 
-SPVM::HTTP::Minimal::Socket - Short Description
+SPVM::HTTP::Minimal::Socket - Socket for HTTP
 
 =head1 Description
 
-The HTTP::Minimal::Socket class of L<SPVM> has methods for someting.
+The HTTP::Minimal::Socket class of L<SPVM> has methods to manipulate a socket for HTTP.
 
 =head1 Usage
 
   use HTTP::Minimal::Socket;
-
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
-=head1 Instance Methods
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
 
 =head1 Copyright & License
 
