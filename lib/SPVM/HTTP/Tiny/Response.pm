5aab860f33495848d3ac05caac681b6cc4c80aeb
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-04 10:58:15 +0900

    Complete rewrite of HTTP::Tiny::Response POD to fix method/field classifications and provide accurate type information

--- lib/SPVM/HTTP/Tiny/Response.pm
+++ lib/SPVM/HTTP/Tiny/Response.pm
@@ -1,64 +1,88 @@
 package SPVM::HTTP::Tiny::Response;
 
-
-
 1;
 
 =head1 Name
 
-SPVM::HTTP::Tiny::Response - HTTP Response
+SPVM::HTTP::Tiny::Response - HTTP Response Object
 
 =head1 Description
 
-The HTTP::Tiny::Response class of L<SPVM> has methods to manipulate HTTP responses.
+The L<HTTP::Tiny::Response|SPVM::HTTP::Tiny::Response> class in L<SPVM> represents an HTTP response received by L<HTTP::Tiny|SPVM::HTTP::Tiny>.
+
+This class provides an interface compatible with Perl's C<HTTP::Tiny> response hash, while also allowing access to the underlying L<Mojo::Message::Response|SPVM::Mojo::Message::Response> object.
 
 =head1 Usage
-  
-  my $response = HTTP::Tiny->new->get("http://example.com/");
-  
-  my $success = $response->success;
-  
-  my $status = $response->status;
-  
-  my $content = $response->content;
+
+  my $res = $http->get($ctx, "http://example.com/");
+
+  # Check if the request was successful (status 2xx)
+  if ($res->success) {
+    # Get status code and reason phrase
+    my $status = $res->status;   # 200
+    my $reason = $res->reason;   # "OK"
+
+    # Get response headers as a Hash
+    my $headers = $res->headers;
+    my $type = (string)$headers->get("content-type");
+
+    # Get response body
+    my $content = $res->content;
+    
+    # Access underlying Mojo object for advanced features
+    my $mojo_res = $res->res;
+    my $version = $mojo_res->version;
+  }
 
 =head1 Fields
 
+=head2 res
+
+C<has res : ro L<Mojo::Message::Response|SPVM::Mojo::Message::Response>;>
+
+The underlying L<Mojo::Message::Response|SPVM::Mojo::Message::Response> object. You can use this field to access low-level response data not provided by the C<HTTP::Tiny::Response> wrapper.
+
+=head1 Instance Methods
+
 =head2 protocol
 
-C<has protocol : ro string;>
+C<method protocol : string ();>
 
-The protocol of the HTTP response.
+Returns the HTTP protocol string, such as C<HTTP/1.1>.
 
 =head2 status
 
-C<has status : ro string;>
+C<method status : int ();>
 
-The status code of the HTTP response.
+Returns the HTTP status code (e.g., C<200>, C<404>).
 
 =head2 success
 
-C<has success : ro byte;>
+C<method success : int ();>
 
-The success field of the response will be true if the status code is 2XX.
+Returns C<1> if the L</"status"> is a C<2xx> code, C<0> otherwise.
 
 =head2 reason
 
-C<has reason : ro string;>
+C<method reason : string ();>
 
-The reason of the status code of the HTTP response.
+Returns the HTTP status message (e.g., C<OK>, C<Not Found>).
 
-=head1 Instance Methods
+=head2 headers
+
+C<method headers : L<Hash|SPVM::Hash> ();>
+
+Returns the response headers as a L<Hash|SPVM::Hash> object.
+The keys are normalized to lower-case. Values are typically strings, but can be string arrays if a header has multiple values.
 
 =head2 content
 
 C<method content : string ();>
 
-Returns the content body of the response.
+Returns the response body as a C<string>.
 
 =head1 Copyright & License
 
-Copyright (c) 2023 Yuki Kimoto
+Copyright (c) 2026 Yuki Kimoto
 
 MIT License
-
